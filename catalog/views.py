0f1f7506bbd65f5076b36b7329670f817a6f490f
Garrett Neuenkirchen <garrettn@gmail.com>
2019-02-24 14:12:34 -0600

    Added session to template to count visits

--- catalog/views.py
+++ catalog/views.py
@@ -12,16 +12,21 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
+    num_visits = request.session.get('num_visits', 0)
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+
+    # we can mess with the sessions here.
+    num_visits = request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits' : num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
